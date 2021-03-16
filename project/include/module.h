fd0ace7fd37835b41ad24aaef2b9ac9e4ca704c8
Georgiy <dev.georgiy@gmail.com>
2021-03-16 13:19:14 +0300

    main code: described the interface

--- project/include/module.h
+++ project/include/module.h
@@ -1,6 +1,41 @@
 #ifndef TP_CCPP_HW1_MODULE_H
 #define TP_CCPP_HW1_MODULE_H
 
-void test_redirect();
+/**
+ * A single-linked list node that stores country parameters and a pointer to the next element.
+ */
+typedef struct country_node country_node;
+/**
+ * Creates a new node.
+ * @return new node or NULL.
+ */
+country_node *create_node();
+/**
+ * Frees up the memory allocated to the node.
+ * @param node - node to free. If it is null, then the function does nothing.
+ */
+void free_node(country_node *node);
+
+/**
+ * Creates a node and reads the country parameters to it from stdin
+ * @return created node or NULL
+ */
+country_node *read_country_node();
+/**
+ * Adds node_to_add to the linked list before the first node greater than or equal to node_to_add.
+ * @param head - pointer to the first item in the list. If it is null, then the function does nothing.
+ * @param node_to_add - pointer to the element to add. If it is null, then the function does nothing.
+ */
+void insert_node(country_node *head, country_node *node_to_add);
+/**
+ * Outputs the country parameters to stdout.
+ * @param head - pointer to the first item in the list. If it is null, then the function does nothing.
+ */
+void print_list(country_node *head);
+/**
+ * Frees up the memory allocated for the list nodes.
+ * @param head - pointer to the first item in the list. If it is null, then the function does nothing.
+ */
+void free_list(country_node *head);
 
 #endif //TP_CCPP_HW1_MODULE_H
--- project/src/module.c
+++ project/src/module.c
@@ -2,9 +2,23 @@
 #include <stdlib.h>
 #include "module.h"
 
-void test_redirect() {
-    char *buf;
-    scanf("%ms", &buf);
-    printf("Прочитал такое: %s", buf );
-    free(buf);
+struct country_node {
+    struct {
+        size_t square;
+        size_t population;
+        char *country_name;
+    } data;
+    country_node *next;
+};
+
+country_node *create_node() {
+    return calloc(1, sizeof(country_node));
+}
+
+void free_node(country_node *node) {
+    if (!node) { return; }
+    if (node->data.country_name) {
+        free(node->data.country_name);
+    }
+    free(node);
 }
