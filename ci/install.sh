a71b32c58f3c7ca943b09f8380ddcd958bcdb6b7
Georgiy <dev.georgiy@gmail.com>
2021-03-18 16:57:32 +0300

    ci: fix-5

--- ci/install.sh
+++ ci/install.sh
@@ -7,6 +7,7 @@ sudo apt-get install --yes valgrind
 sudo apt-get install --yes cppcheck
 sudo apt-get install --yes curl
 sudo apt-get install --yes wget
+sudo apt-get install --yes unzip
 export CODECOV_TOKEN="f595c59d-c930-48e3-8c8c-dbff8bd00453"
 cd /usr/src/gtest && sudo mkdir build && cd build 
 sudo cmake .. && sudo make && sudo cp *.a /usr/lib
